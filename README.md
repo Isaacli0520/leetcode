dcc5b4f5ba69f2ef1cd6fc35fb8547ec97d22a14
Isaac <il5fq@virginia.edu>
2021-09-14 16:55:38 -0400

    130 & 200 & readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # leetcode
+
+## Things I don't Know
+
+### Python map() function
+map applies the function to all the items in the input iterable and returns an iterator
+```python
+>>> list(map(lambda x: x + 1, [1, 2, 3])) 
+[2, 3, 4]
+
+>>> list(map(lambda x, y: x + y, [1, 2, 3], [3, 4, 5])) 
+[4, 6, 8]
+```
--- dfs/0200_number_of_islands.py
+++ dfs/0200_number_of_islands.py
@@ -3,17 +3,19 @@ class Solution:
         if not grid:
             return 0
         count = 0
+        r, c = len(grid), len(grid[0])
+        
+        def dfs(i, j):
+            if i < 0 or i >= r or j < 0 or j >= c or grid[i][j] != "1":
+                return
+            grid[i][j] = "#"
+            for m, n in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+                dfs(m, n)
+        
         for i in range(r):
             for j in range(c):
                 if grid[i][j] == "1":
-                    self.dfs(i, j, grid)
+                    dfs(i, j)
                     count += 1
         return count
-                
-    def dfs(self, i, j, board):
-        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != "1":
-            return
-        board[i][j] = "#"
-        for m, n in [(i, j - 1),(i - 1, j),(i + 1,j),(i,j + 1)]:
-            self.dfs(m, n, board)
-        
+    
