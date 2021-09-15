673e9b5e4713091dd71da9077b0dfcb78e8f0630
Isaac <il5fq@virginia.edu>
2021-09-15 13:09:18 -0400

    695

--- dfs/0695_max_area_of_island.py
+++ dfs/0695_max_area_of_island.py
@@ -1,17 +1,18 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return 0
         r, c = len(grid), len(grid[0])
-        visited = [[0 for j in range(c)] for i in range(r)]
+        
         def dfs(i, j):
-            if i < 0 or i >= r or j < 0 or j >= c or not grid[i][j] or visited[i][j]:
-                return 0
-            visited[i][j] = 1
-            return 1 + dfs(i - 1, j) + dfs(i, j - 1) + dfs(i + 1, j) + dfs(i, j + 1)
-        ret = 0
+            grid[i][j] = 0
+            area = 1
+            for (m, n) in [(i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j)]:
+                if 0 <= m < r and 0 <= n < c and grid[m][n]:
+                    area += dfs(m, n)
+            return area
+         
+        max_area = 0
         for i in range(r):
             for j in range(c):
-                if not visited[i][j] and grid[i][j]:
-                    ret = max(ret, dfs(i, j))
-        return ret
+                if grid[i][j]:
+                    max_area = max(max_area, dfs(i, j))
+        return max_area
