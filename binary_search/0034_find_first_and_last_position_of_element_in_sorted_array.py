817785fd81b88aeafd2db94050b7a8934d89d863
Isaac <il5fq@virginia.edu>
2021-09-15 16:28:48 -0400

    34

--- binary_search/0034_find_first_and_last_position_of_element_in_sorted_array.py
+++ binary_search/0034_find_first_and_last_position_of_element_in_sorted_array.py
@@ -1,33 +1,40 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        l = len(nums)
+        n = len(nums)
+        left, right = 0, n - 1
         res = [-1, -1]
-        if l == 0: return res
-        left, right = 0, l - 1
-        while(left <= right):
-            mid = int(right + (left - right)/2)
-            if nums[mid] == target:
-                right = mid - 1
-            elif nums[mid] > target:
-                right = mid - 1
-            elif nums[mid] < target:
-                left = mid + 1
-        if left >= l or nums[left] != target:
-            res[0] = -1
-        else:
-            res[0] = left
         
-        left, right = 0, l - 1
-        while(left <= right):
-            mid = int(right + (left - right)/2)
-            if nums[mid] == target:
+        # Find Right
+        # Key diff from normal binary search:
+        #   When nums[mid] == target, shrink left bound
+        while left <= right:
+            mid = int(left + (right - left) / 2)
+            if nums[mid] <= target:
                 left = mid + 1
             elif nums[mid] > target:
                 right = mid - 1
-            elif nums[mid] < target:
-                left = mid + 1
-        if right >= l or nums[right] != target:
+        
+         # Target is less than every element, right = -1
+        if right < 0 or nums[right] != target:
             res[1] = -1
         else:
             res[1] = right
+        
+        # Find Left
+        # Key diff from normal binary search:
+        #   When nums[mid] == target, shrink right bound
+        left, right = 0, n - 1
+        while left <= right:
+            mid = int(left + (right - left) / 2)
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] >= target:
+                right = mid - 1
+            
+        # Target is bigger than every element, left = -1
+        if left >= n or nums[left] != target:
+            res[0] = -1
+        else:
+            res[0] = left
+            
         return res
