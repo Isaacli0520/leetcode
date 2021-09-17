5745beeb794ce679bff67595e45d35a856341865
Isaac <il5fq@virginia.edu>
2021-09-17 12:52:18 -0400

    167

--- binary_search/0167_two_sum_2_sorted_array.py
+++ binary_search/0167_two_sum_2_sorted_array.py
@@ -1,18 +1,31 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        l = len(nums)
-        history = []
-        for i in range(l):
-            if nums[i] not in history:
-                history.append(nums[i])
-                lo, hi = i + 1, l - 1
-                tmp_t = target - nums[i]
-                while lo <= hi:
-                    mid = lo + (hi - lo) // 2
-                    if nums[mid] == tmp_t:
-                        return [i + 1, mid + 1]
-                    elif nums[mid] < tmp_t:
-                        lo = mid + 1
-                    elif nums[mid] > tmp_t:
-                        hi = mid - 1
-        return [-1, -1]
+    # Two Pointers
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            s = numbers[l] + numbers[r]
+            if s == target:
+                return [l + 1, r + 1]
+            elif s < target:
+                l += 1
+            elif s > target:
+                r -= 1
+        
+    # # Binary Search
+    # def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    #     n = len(numbers)
+    #     visited = []
+    #     for i in range(n):
+    #         if numbers[i] not in visited:
+    #             visited.append(numbers[i])
+    #             tmp = target - numbers[i]
+    #             left, right = i + 1, n - 1
+    #             while left <= right:
+    #                 mid = int(left + (right - left) / 2)
+    #                 if numbers[mid] == tmp:
+    #                     return [i + 1, mid + 1] # 1-indexed not 0-indexed
+    #                 elif numbers[mid] > tmp:
+    #                     right = mid - 1
+    #                 elif numbers[mid] < tmp:
+    #                     left = mid + 1
+    #     return [-1, -1]
