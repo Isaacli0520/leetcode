10cd75de6869044d77b97a3dbac9cb88318f89cd
Isaac <il5fq@virginia.edu>
2021-09-17 11:54:27 -0400

    162

--- binary_search/0162_find_peak_element.py
+++ binary_search/0162_find_peak_element.py
@@ -2,10 +2,13 @@ class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         left, right = 0, len(nums) - 1
         while left < right:
-            mid1 = left + (right - left) // 2
-            mid2 = mid1 + 1
-            if nums[mid1] < nums[mid2]:
-                left = mid2
+            mid = int(left + (right - left) / 2)
+            # If mid is increasing, there must be a peak on the right
+            # Because rightmost is -inf
+            if nums[mid] < nums[mid + 1]:
+                left = mid + 1
+            # If mid is decreasing, there must be a peak on the left
+            # Becasue leftmost is - inf
             else:
-                right = mid1
+                right = mid
         return left
