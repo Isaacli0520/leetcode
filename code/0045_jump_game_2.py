a9db9c07dfda38b87ed30b32df94ee5d29aa70b2
Isaac <il5fq@virginia.edu>
2022-04-27 23:50:28 -0400

    2022 Day 25

--- code/0045_jump_game_2.py
+++ code/0045_jump_game_2.py
@@ -19,24 +19,16 @@ class Solution:
                 
         return jumps
         
-    # BFS
-    # [2, 3, 1, 1, 4]
-    # 1 step:
-    #   index 1 and 2
-    # 2 steps:
-    #   index 3 and 4
-    # For each step, update maxend based on every i + nums[i]
-    # update start = end + 1, end = maxend
+    # DP
+    # dp[i] = min number of jumps to reach index i
     def jump(self, nums: List[int]) -> int:
-        l, start, end, step = len(nums), 0, 0, 0
-        # end < l - 1 to cover the case when len(nums) == 1
-        while end < l - 1:
-            step += 1
-            maxend = end + 1
-            for i in range(start, end + 1):
-                if i + nums[i] >= l - 1:
-                    return step
-                maxend = max(maxend, i + nums[i])
-            start, end = end + 1, maxend
-        return step
-    
+        curr_maxi = 0
+        dp = list(range(len(nums)))
+        for i, num in enumerate(nums):
+            if num + i < curr_maxi:
+                continue
+            else:
+                curr_maxi = num + i
+            for j in range(1, min(num + 1, len(nums) - i)):
+                dp[i + j] = min(dp[i] + 1, dp[i + j])
+        return dp[-1]
