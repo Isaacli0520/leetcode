c37e1de0c9378988f5882442c544e38e791ce143
Isaac <il5fq@virginia.edu>
2022-05-11 23:55:55 -0400

    2022 Day 34

--- code/0230_kth_smallest_element_in_a_bst.py
+++ code/0230_kth_smallest_element_in_a_bst.py
@@ -5,6 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    # Recursive
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        res = -1
+        def helper(node):
+            nonlocal res
+            nonlocal k
+            if not node or k <= 0:
+                return
+            helper(node.left)
+            k -= 1
+            if k == 0:
+                res = node.val
+                return
+            helper(node.right)
+        helper(root)
+        return res
+
+    # Iterative
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         curr = root
         stack = []
