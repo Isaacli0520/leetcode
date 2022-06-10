32a0a96c3490915cc0f5fe16841fead5617b0f12
Isaac <il5fq@virginia.edu>
2022-06-10 18:35:32 -0400

    2022 Day 45

--- code/1472_design_browser_history.py
+++ code/1472_design_browser_history.py
@@ -1,4 +1,30 @@
 class BrowserHistory:
+    # O(1) time complexity
+    # Use a bound variable to store the end of current history
+    # so that there is no need to pop every history that comes after
+    # current page
+    def __init__(self, homepage: str):
+        self.history = [homepage]
+        self.curr = 0
+        self.end = 0
+
+    def visit(self, url: str) -> None:
+        if self.curr == len(self.history) - 1:
+            self.history.append(url)
+        else:
+            self.history[self.curr + 1] = url
+        self.curr += 1
+        self.end = self.curr
+            
+
+    def back(self, steps: int) -> str:
+        self.curr -= min(steps, self.curr)
+        return self.history[self.curr]
+
+    def forward(self, steps: int) -> str:
+        self.curr += min(steps, self.end - self.curr)
+        return self.history[self.curr]
+
     # Using two stacks
     def __init__(self, homepage: str):
         self.history = [homepage]
@@ -20,24 +46,37 @@ class BrowserHistory:
             self.history.append(self.fwd.pop())
         return self.history[-1]
 
-    # Using one array and one pointer
+# Doubly-Linked List
+class BrowserHistory:
+
     def __init__(self, homepage: str):
-        self.stack = [homepage]
-        self.pointer = 0
+        self.curr = Node(homepage)
 
     def visit(self, url: str) -> None:
-        self.stack = self.stack[:self.pointer + 1]
-        self.stack.append(url)
-        self.pointer += 1
+        new_node = Node(url)
+        self.curr.next = new_node
+        new_node.prev = self.curr
+        self.curr = self.curr.next
 
     def back(self, steps: int) -> str:
-        self.pointer -= min(steps, self.pointer)
-        return self.stack[self.pointer]
+        for i in range(steps):
+            if not self.curr.prev:
+                return self.curr.val
+            self.curr = self.curr.prev
+        return self.curr.val
 
     def forward(self, steps: int) -> str:
-        self.pointer += min(len(self.stack) - 1 - self.pointer, steps)
-        return self.stack[self.pointer]
+        for i in range(steps):
+            if not self.curr.next:
+                return self.curr.val
+            self.curr = self.curr.next
+        return self.curr.val
 
+class Node:
+    def __init__(self, val, prev=None, nxt=None):
+        self.val = val
+        self.prev = prev
+        self.next = nxt
 
 # Your BrowserHistory object will be instantiated and called as such:
 # obj = BrowserHistory(homepage)
